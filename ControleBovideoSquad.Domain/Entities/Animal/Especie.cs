85186a141ce64ccd0d20fe1d58d326ac9897d534
Kevin Olentino <kevinolentino@gmail.com>
2021-11-30 11:25:43 -0400

    Update Especie.cs

--- ControleBovideoSquad.Domain/Entities/Animal/Especie.cs
+++ ControleBovideoSquad.Domain/Entities/Animal/Especie.cs
@@ -2,7 +2,19 @@ namespace ControleBovideoSquad.Domain.Entities
 {
     public class Especie
     {
+
         public virtual int IdEspecie { get; protected set; }
-        public virtual string? Nome { get; protected set; }
+        public virtual string Nome { get; protected set; }
+
+        protected Especie()
+        {
+
+        }
+
+        public Especie(int idEspecie, string nome)
+        {
+            IdEspecie = idEspecie;
+            Nome = nome;
+        }
     }
 }
