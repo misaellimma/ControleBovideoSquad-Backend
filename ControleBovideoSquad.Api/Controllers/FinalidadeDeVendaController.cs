8236c0b8fee84edf435637e7fe96c3392ad63907
Kevin Olentino <kevinolentino@gmail.com>
2021-11-30 13:05:36 -0400

    Finalidade de Venda: Controller, Service

---
+++ ControleBovideoSquad.Api/Controllers/FinalidadeDeVendaController.cs
@@ -0,0 +1,30 @@
+﻿using ControleBovideoSquad.Application.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleBovideoSquad.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FinalidadeDeVendaController : ControllerBase
+    {
+        private readonly IFinalidadeDeVendaService _finalidadeDeVendaService;
+
+        public FinalidadeDeVendaController(IFinalidadeDeVendaService finalidadeDeVendaService)
+        {
+            this._finalidadeDeVendaService = finalidadeDeVendaService;
+        }
+
+        [HttpGet]
+        public IActionResult ObterTodos()
+        {
+            return Ok(_finalidadeDeVendaService.ObterTodos());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            return Ok(_finalidadeDeVendaService.Obter(id));
+        }
+    }
+}
