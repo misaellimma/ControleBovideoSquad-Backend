4472156cd122da5e1ee3dd4017f0194d48e62c68
Kevin Olentino <kevinolentino@gmail.com>
2021-11-30 10:03:50 -0400

    Adicionando Pattern UnityOfWork

---
+++ ControleBovideoSquad.Repository/Interfaces/IUnityOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleBovideoSquad.Repository.Interfaces
+{
+    public interface IUnityOfWork : IDisposable
+    {
+        void Commit();
+        void SaveOrUpdate<T>(T entity);
+        void Delete<T>(T entity); 
+        IQueryable<T> Query<T>();
+    }
+}
