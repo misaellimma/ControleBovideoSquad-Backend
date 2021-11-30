950a3c9961dc08b74b239c380441e092f5116e76
Kevin Olentino <kevinolentino@gmail.com>
2021-11-30 17:09:11 -0400

    RegistroVacina Controller, Model, Service e Mapper

---
+++ ControleBovideoSquad.Api/Controllers/RegistroVacinaController.cs
@@ -0,0 +1,46 @@
+﻿using ControleBovideoSquad.Application.IServices;
+using ControleBovideoSquad.CrossCutting.Dto.RegistroVacina;
+using ControleBovideoSquad.CrossCutting.Util;
+using ControleBovideoSquad.Domain.Entities.Animal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleBovideoSquad.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistroVacinaController : ControllerBase
+    {
+        private readonly IRegistroVacinaService _registroVacinaService;
+
+        public RegistroVacinaController(IRegistroVacinaService registroVacinaService)
+        {
+            this._registroVacinaService = registroVacinaService;
+        }
+
+        [HttpDelete]
+        public IActionResult Cancelar(int id)
+        {
+            var response = _registroVacinaService.Cancelar(id);
+            if (response.Errors.Any()) 
+                return BadRequest(response.Errors);
+
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Incluir(RegistroVacinaDto registroVacina)
+        {
+            var response = _registroVacinaService.Incluir(registroVacina);
+
+            if (response.Errors.Any())
+                return BadRequest(response.Errors);
+
+            return Ok();
+        }
+        [HttpGet("{inscricaoEstadual}")]
+        public IActionResult ObterPorPropriedade(string inscricaoEstadual)
+        {
+            return Ok(_registroVacinaService.ObterPorPropriedade(inscricaoEstadual));
+        }
+    }
+}
---
+++ ControleBovideoSquad.Application/Services/Animais/RegistroVacinaService.cs
@@ -0,0 +1,82 @@
+﻿using ControleBovideoSquad.Application.IMapper;
+using ControleBovideoSquad.Application.IServices;
+using ControleBovideoSquad.CrossCutting.Dto.RegistroVacina;
+using ControleBovideoSquad.CrossCutting.Util;
+using ControleBovideoSquad.Domain.Entities.Animal;
+using ControleBovideoSquad.Domain.Repositories.Animais;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleBovideoSquad.Application.Services.Animais
+{
+    public class RegistroVacinaService : IRegistroVacinaService
+    {
+        private readonly IRegistroVacinaRepository _registroVacinaRepository;
+        private readonly IMapper<RegistroVacinaDto, RegistroVacina> _registroMapper;
+        private readonly IRebanhoRepository _rebanhoRepository;
+
+        public RegistroVacinaService(IRegistroVacinaRepository registroVacinaRepository,
+            IMapper<RegistroVacinaDto, RegistroVacina> registroMapper,
+            IRebanhoRepository rebanhoRepository
+            )
+        {
+            _registroVacinaRepository = registroVacinaRepository;
+            _registroMapper = registroMapper;
+            _rebanhoRepository = rebanhoRepository;
+        }
+
+        public Result<string> Cancelar(int id)
+        {
+            RegistroVacina? registroVacina= _registroVacinaRepository.Obter(id);
+
+            if (registroVacina == null)
+                return Result<string>.Error("");
+
+            registroVacina.Cancelar();
+            _registroVacinaRepository.Save(registroVacina);
+
+            return Result<string>.Success("");
+        }
+
+        public Result<RegistroVacina> Incluir(RegistroVacinaDto registroVacinaDto)
+        {
+            var validation = TempRegistroValidation(registroVacinaDto);
+
+            if(validation.Any())
+                return Result<RegistroVacina>.Error(validation);
+
+            _registroVacinaRepository.Save(_registroMapper.MapearDtoParaEntidade(registroVacinaDto));
+
+
+            return Result<RegistroVacina>.Success(_registroMapper.MapearDtoParaEntidade(registroVacinaDto));
+        }
+
+        public List<RegistroVacina> ObterPorPropriedade(string inscricaoEstadual)
+        {
+            return _registroVacinaRepository.ObterPorPropriedade(inscricaoEstadual);
+        }
+        
+        public List<string> TempRegistroValidation(RegistroVacinaDto registroVacinaDto)
+        {
+            List<string> error = new List<string>();
+
+            if(registroVacinaDto.Quantidade <= 0)
+            {
+                error.Add("Quantidade não pode ser nula!");
+            }
+            if(registroVacinaDto.IdVacina <= 0)
+            {
+                error.Add("Vacina não pode ser nula!");
+            }
+            if (registroVacinaDto.IdRebanho <= 0)
+            {
+                error.Add("Rebanho não pode ser nulo!");
+            }
+            
+            return error;
+        }
+    }
+}
