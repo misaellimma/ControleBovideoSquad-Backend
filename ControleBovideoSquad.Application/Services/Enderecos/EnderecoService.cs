bb4d18c287f18350c833f8166168027fc1944815
Kevin Olentino <kevinolentino@gmail.com>
2021-11-30 11:25:39 -0400

    Adicionando EnderecoService, Corrigindo merge em Endereco

---
+++ ControleBovideoSquad.Application/Services/Enderecos/EnderecoService.cs
@@ -0,0 +1,63 @@
+﻿using ControleBovideoSquad.Application.IServices.Enderecos;
+using ControleBovideoSquad.CrossCutting.Util;
+using ControleBovideoSquad.Domain.Entities.Enderecos;
+using ControleBovideoSquad.Domain.Repositories.Enderecos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleBovideoSquad.Application.Services.Enderecos
+{
+    public class EnderecoService : IEnderecoService
+    {
+        private readonly IEnderecoRepository _enderecoRepository;
+
+        public EnderecoService(IEnderecoRepository enderecoRepository)
+        {
+            this._enderecoRepository = enderecoRepository;
+        }
+
+        public Endereco Obter(int id)
+        {
+            return this._enderecoRepository.ObterEnderecoPorID(id);
+        }
+
+        public List<Endereco> ObterTodos()
+        {
+            return this._enderecoRepository.ObterEnderecos();
+        }
+
+        public Result<Endereco> Save(Endereco endereco)
+        {
+            var response = TempClassToValidate(endereco);
+
+            if(response.Any())
+                return Result<Endereco>.Error(response);
+
+            return Result<Endereco>.Success(endereco);
+
+        }
+
+        public List<string> TempClassToValidate(Endereco endereco)
+        {
+            List<string> errors = new List<string>();
+
+            if (endereco.Rua == null || endereco.Numero == null || endereco.Municipio.IdMunicipio == 0)
+            {
+                errors.Add("Rua, Endereco, ou IdMuncipio não pode ser nulo!");
+            }
+            if(endereco.Rua.Length > 200)
+            {
+                errors.Add("Rua não pode se rmaior que 250 caracteres");
+            }
+            if (endereco.Numero.Length > 20)
+            {
+                errors.Add("Numero não pode ser maior que 20 caracteres");
+            }            
+
+            return errors;
+        }
+    }
+}
--- ControleBovideoSquad.Domain/Entities/Endereco/Endereco.cs
+++ ControleBovideoSquad.Domain/Entities/Endereco/Endereco.cs
@@ -5,13 +5,15 @@ namespace ControleBovideoSquad.Domain.Entities.Enderecos
     public class Endereco
     {
         public virtual int IdEndereco { get; protected set; }
-        public virtual string? Rua { get; protected set; }
-        public virtual string? Numero { get; protected set; }
-        public virtual Municipio? Municipio { get; protected set; }
+        public virtual string Rua { get; protected set; }
+        public virtual string Numero { get; protected set; }
+        public virtual Municipio Municipio { get; protected set; }
 
         protected Endereco()
         {
-
+            Rua = "";
+            Numero = "";
+            Municipio = new Municipio(0,"","");
         }
 
         public Endereco(int idEndereco, string rua, string numero, Municipio municipio)
