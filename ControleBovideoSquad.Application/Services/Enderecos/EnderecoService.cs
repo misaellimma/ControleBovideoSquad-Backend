7206fc4ba589e03610b040637ace7e6a6cfdf9ba
Kevin Olentino <kevinolentino@gmail.com>
2021-12-01 15:14:04 -0400

    Update EnderecoService.cs

--- ControleBovideoSquad.Application/Services/Enderecos/EnderecoService.cs
+++ ControleBovideoSquad.Application/Services/Enderecos/EnderecoService.cs
@@ -1,14 +1,11 @@
 ﻿using ControleBovideoSquad.Application.IMapper;
 using ControleBovideoSquad.Application.IServices.Enderecos;
+using ControleBovideoSquad.Application.Validators;
+using ControleBovideoSquad.CrossCutting;
 using ControleBovideoSquad.CrossCutting.Dto.EnderecoDto;
 using ControleBovideoSquad.CrossCutting.Util;
 using ControleBovideoSquad.Domain.Entities.Enderecos;
 using ControleBovideoSquad.Domain.Repositories.Enderecos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ControleBovideoSquad.Application.Services.Enderecos
 {
@@ -16,11 +13,15 @@ namespace ControleBovideoSquad.Application.Services.Enderecos
     {
         private readonly IEnderecoRepository _enderecoRepository;
         private readonly IMapper<EnderecoDto, Endereco> _enderecoMapper;
+        private readonly IValidator<EnderecoDto> enderecoValidator;
 
-        public EnderecoService(IEnderecoRepository enderecoRepository, IMapper<EnderecoDto, Endereco> enderecoMapper)
+        public EnderecoService(IEnderecoRepository enderecoRepository, 
+            IMapper<EnderecoDto, Endereco> enderecoMapper,
+            IValidator<EnderecoDto> enderecoValidator)            
         {
             _enderecoRepository = enderecoRepository;
             _enderecoMapper = enderecoMapper;
+            this.enderecoValidator = enderecoValidator;
         }
 
         public Endereco Obter(int id)
@@ -33,36 +34,17 @@ namespace ControleBovideoSquad.Application.Services.Enderecos
             return this._enderecoRepository.ObterEnderecos();
         }
 
-        public Result<Endereco> Save(EnderecoDto endereco)
+        public Result<Endereco> Save(EnderecoDto enderecoDto)
         {
-            var response = TempClassToValidate(endereco);
+            var validator = enderecoValidator.IsValid(enderecoDto);
 
-            if(response.Any())
-                return Result<Endereco>.Error(response);
+            if(validator.Count > 0)
+                return Result<Endereco>.Error(EStatusCode.BAD_REQUEST, validator);
 
-            _enderecoRepository.Save(_enderecoMapper.MapearDtoParaEntidade(endereco));
-            return Result<Endereco>.Success(_enderecoMapper.MapearDtoParaEntidade(endereco));
+            _enderecoRepository.Save(_enderecoMapper.MapearDtoParaEntidade(enderecoDto));
+            return Result<Endereco>.Success(_enderecoMapper.MapearDtoParaEntidade(enderecoDto));
 
         }
-
-        public List<string> TempClassToValidate(EnderecoDto endereco)
-        {
-            List<string> errors = new List<string>();
-
-            if (endereco.Rua == null || endereco.Numero == null || endereco.IdMunicipio == 0)
-            {
-                errors.Add("Rua, Endereco, ou IdMuncipio não pode ser nulo!");
-            }
-            if(endereco.Rua.Length > 200)
-            {
-                errors.Add("Rua não pode se rmaior que 250 caracteres");
-            }
-            if (endereco.Numero.Length > 20)
-            {
-                errors.Add("Numero não pode ser maior que 20 caracteres");
-            }            
-
-            return errors;
-        }
+       
     }
 }
