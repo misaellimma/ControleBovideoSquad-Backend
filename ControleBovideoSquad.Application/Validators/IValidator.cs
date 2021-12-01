cf104f022fac9afdffec36fcbe3b497e534fb7b6
Kevin Olentino <kevinolentino@gmail.com>
2021-12-01 15:13:55 -0400

    Validators

---
+++ ControleBovideoSquad.Application/Validators/IValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleBovideoSquad.Application.Validators
+{
+    public interface IValidator<T>
+    {
+        public List<string> IsValid(T value);
+        public List<string> errors {get;}
+    }
+}
